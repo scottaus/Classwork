9487d5be8a275890ea6838ae20e43cae72eb5992
unknown <scottaus002@warren.k12.in.us>
2018-12-06 13:27:27 -0500

    commit

---
+++ jQuery/javascript.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $(".clickme").click(function () {
+        $(".hideme").css
+        ("background-color","red", )
+        });
+    
+
+})
+
+//functions covered
+/*
+    .show()
+    .hide()
+    .toggle()
+    .fadeIn()
+    .fadeOut()
+    .fadeToggle()
+    .fadeTo()
+    .on()
+    .slideDown()
+    .slideUp()
+    .slideToggle()
+
+*/
